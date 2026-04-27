f1aaaed5c0ba122fe9cc89b975a2d9114ef6714b
Armontex <windle1337@gmail.com>
2026-04-28 00:38:11 +0500

    add gitignore

--- init_backend.sh
+++ init_backend.sh
@@ -2,16 +2,41 @@ uv python pin 3.13
 uv init
 uv venv
 
-touch Dockerfile
+# -----------------
+
 touch docker-compose.yml
 touch .env.example
 touch AGENTS.md
 touch TODO.md
 
+curl -L -o Dockerfile \
+    https://raw.githubusercontent.com/Armontex/base/refs/heads/main/Dockerfile
+
 curl -L -o .pre-commit-config.yaml \
-  https://raw.githubusercontent.com/Armontex/base/refs/heads/main/.pre-commit-config.yaml
+    https://raw.githubusercontent.com/Armontex/base/refs/heads/main/.pre-commit-config.yaml
+
+# -----------------
 
 uv add --dev pre-commit pyright commitizen
 uv run pre-commit autoupdate
 uv run pre-commit install --install-hooks --hook-type pre-commit --hook-type commit-msg
 uv run pre-commit run --all-files
+
+# -----------------
+
+uv add alembic
+alembic init migrations
+
+# -----------------
+
+
+
+# -----------------
+
+grep -q '^\[tool\.pyright\]$' pyproject.toml || cat >> pyproject.toml <<'EOF'
+
+[tool.pyright]
+venvPath = "."
+venv = ".venv"
+pythonVersion = "3.13"
+EOF
